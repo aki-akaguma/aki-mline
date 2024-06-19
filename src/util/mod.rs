3d99450fc51aa46dcf67963b5494b50863e58e7e
aki <aki.akaguma@hotmail.com>
2024-06-19 21:37:30 +0900

    fixed clippy: `unused_imports`

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,12 +2,12 @@ pub mod err;
 
 mod opt_uc_x_param;
 pub use self::opt_uc_x_param::OptUcXParam;
-pub use self::opt_uc_x_param::OptUcXParamParseError;
+//pub use self::opt_uc_x_param::OptUcXParamParseError;
 
 mod opt_color_when;
 pub use self::opt_color_when::OptColorWhen;
-pub use self::opt_color_when::OptColorWhenParseError;
+//pub use self::opt_color_when::OptColorWhenParseError;
 
 mod opt_around_num;
 pub use self::opt_around_num::OptAroundNum;
-pub use self::opt_around_num::OptAroundNumParseError;
+//pub use self::opt_around_num::OptAroundNumParseError;
